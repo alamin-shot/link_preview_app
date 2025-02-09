035d1f43ad30f52c34200de7447af87d288cf9ea
Al Amin <rifat00175@gmail.com>
2025-02-10 01:59:17 +0600

    Updated metadata and content

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+
+export async function getServerSideProps() {
+	return {
+		props: {},
+	};
+}
 export default function Home() {
 	return (
 		<>
